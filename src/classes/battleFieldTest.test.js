df207c03872f477ad41ef814564df38c87cb0452
snooze26 <langdon.douglas26@gmail.com>
2025-04-28 18:02:16 -0500

    test: added failed test for validLocation

--- src/classes/battleFieldTest.test.js
+++ src/classes/battleFieldTest.test.js
@@ -1,7 +1,13 @@
 import {Battle_Field} from "./battleFieldClass.js"
 
-test("Location is valid?" , () => {
+test("Location is valid" , () => {
     const bfTest = new Battle_Field(8 , 8);
     const testPosition = [6 , 7];
     expect(bfTest.validLocation(testPosition)).toBe(true); 
+});
+
+test("Location is inValid" , () => {
+    const bfTest = new Battle_Field(8 , 8);
+    const testPosition = [9 , 9];
+    expect(bfTest.validLocation(testPosition)).toBe(false); 
 });
