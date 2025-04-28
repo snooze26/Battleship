f60fc3c1a9c3e10d8b879f48141b32d8793039b9
snooze26 <langdon.douglas26@gmail.com>
2025-04-28 17:50:57 -0500

    sideThing: setUp jest and babel on laptop

--- src/classes/battleFieldTest.js
+++
@@ -1,7 +0,0 @@
-import * as Battle_Field from "./battleFieldClass.js"
-
-test("Location is valid?" , () => {
-    const testBfieldd = new Battle_Field(8, 8);
-    const testPosition = [6 , 7];
-    expect(validLocation(testPosition)).toBe(true); 
-});
