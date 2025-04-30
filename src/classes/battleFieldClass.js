494c3a15a5ddff1b51b3f4177d0604d538646a15
snooze26 <langdon.douglas26@gmail.com>
2025-04-29 20:14:03 -0500

    Added and test getCoords function

--- src/classes/battleFieldClass.js
+++ src/classes/battleFieldClass.js
@@ -1,3 +1,5 @@
+import { error } from "console";
+
 export class Battle_Field { 
     bField; 
     attackCords = [];
@@ -22,8 +24,30 @@ export class Battle_Field {
         if((x >= 0 && x < boardLimit) && (y >= 0 && y < boardLimit)) return true
         return false 
     }
+
+    getCoords(input) { 
+        // check if input is not a string 
+        if(typeof input !== "string") return console.error("Please enter a valid location 'x,x");
+        
+        //split coords into two elements 
+        const coords = input.split('-');
+        if(!coords == 2) return console.error("Please enter a valid location 'x");
+
+        //change coords to numbers
+        const xNumber = parseInt(coords[0].trim(), 10);
+        const yNumber = parseInt(coords[1].trim(), 10);
+
+        //chech if coords are number 
+        if(!Number.isInteger(xNumber)) return console.error('Please list a valid x coordinate');
+        if(!Number.isInteger(yNumber)) return console.error('Please list a valid y coordinate');
+        
+        // return coords
+        return [xNumber , yNumber]; 
+    }
 }
 
 
-// const testBfieldd = new Battle_Field(10, 10); 
-// console.log(testBfieldd);
+const testBfieldd = new Battle_Field(10, 10); 
+const input = '10-10';
+
+console.log(testBfieldd.getCoords(input));
