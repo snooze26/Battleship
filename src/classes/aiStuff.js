59c377bcc4a401da14fc123162265f82ce0c4503
snooze26 <langdon.douglas26@gmail.com>
2025-05-19 19:00:24 -0500

    feat: tested and made logical changes to getStateOfGame

--- src/classes/aiStuff.js
+++ src/classes/aiStuff.js
@@ -1,10 +1,13 @@
+const Battle_Field = require('./battleFieldClass.js');
+const Ships = require('./shipClass.js')
+
 function getStateOfGame(playerBoard) { 
     const gameFeed = []; 
+    
 
-    for(let y = 0; y < playerBoard.length; y++) {
-        for(let x = 0; x < playerBoard.length; x++) { 
-            const cell = playerBoard[x][y]; 
-
+    for(let y = 0; y < playerBoard.row; y++) {
+        for(let x = 0; x < playerBoard.row; x++) { 
+            const cell = playerBoard.bField[x][y]; 
             if(cell === "H" || cell === "M" ) { 
                 gameFeed.push({
                     x, 
@@ -15,8 +18,26 @@ function getStateOfGame(playerBoard) {
         }
     }
 
+    // console.log(gameFeed);
     return { 
-        boardSize : playerBoard.length;
-        gameHistory : gameFeed;
+        boardSize : playerBoard.row,
+        gameHistory : gameFeed
     }
+    
 }
+
+
+//mock data 
+const bfTest = new Battle_Field(10 , 10); 
+const testCoords = [[4 ,3], [4 ,4] , [4 ,5] , [4 ,6] , [4 ,7]]; 
+const [x , y] = testCoords[0]; 
+const testShip = new Ships.Carrier();
+bfTest.placeShip(testCoords , testShip); 
+bfTest.attack([4 ,3]);
+bfTest.attack([4 ,5]);
+bfTest.attack([4 , 1]);
+
+console.log(bfTest.bField)
+
+console.log(getStateOfGame(bfTest));
+
