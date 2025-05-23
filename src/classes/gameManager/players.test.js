0a1b287cfdfd4d28b33aa8794d3416db29830057
snooze26 <langdon.douglas26@gmail.com>
2025-05-22 19:25:50 -0500

    test: test for when player misses on their turn

--- src/classes/gameManager/players.test.js
+++ src/classes/gameManager/players.test.js
@@ -4,28 +4,46 @@ const AI = require('../aiStuff.js');
 const Players = require('./players.js')
 
 
-test("Ship has been attacked by player and marked as 'H' " , () => {
+// test("Ship has been attacked by player and marked as 'H' " , () => {
 
-const humanPlayer = new Players.HumanPlayer("Tony the tiger"); 
-const computerPlayer = new Players.ComputerPlayer("Tony the bot " , "easy");
+// const humanPlayer = new Players.HumanPlayer("Tony the tiger"); 
+// const computerPlayer = new Players.ComputerPlayer("Tony the bot " , "easy");
 
-const testPatrol = new Ships.Patrol(); 
-const testPatrolCoords = [[4,4] , [4 , 5]]; 
+// const testPatrol = new Ships.Patrol(); 
+// const testPatrolCoords = [[4,4] , [4 , 5]]; 
 
+// // place ships on computer player's board 
+// computerPlayer.battleField.placeShip( testPatrolCoords , testPatrol);
 
-// place ships on computer player's board 
-computerPlayer.battleField.placeShip( testPatrolCoords , testPatrol);
+// const attackCoords = [4,4];
 
-const carrier = new Ships.Carrier();
-const battleShip = new Ships.Battleship(); 
+// //human player attacks 
+// humanPlayer.takeTurn(computerPlayer, attackCoords);
 
-const attackCoords = [4,4];
+// const resultCell = computerPlayer.battleField.getBattleField()[4][4]
 
-//human player attacks 
-humanPlayer.takeTurn(computerPlayer, attackCoords);
+// expect(resultCell).toBe("H")
 
-const resultCell = computerPlayer.battleField.getBattleField()[4][4]
+// })
 
-expect(resultCell).toBe("H")
+test("Ship has been attacked by player and marked as 'M' " , () => {
 
-})
+    const humanPlayer = new Players.HumanPlayer("Tony the tiger"); 
+    const computerPlayer = new Players.ComputerPlayer("Tony the bot " , "easy");
+    
+    const testPatrol = new Ships.Patrol(); 
+    const testPatrolCoords = [[4,4] , [4 , 5]]; 
+      
+    // place ships on computer player's board 
+    computerPlayer.battleField.placeShip( testPatrolCoords , testPatrol);
+    
+    const attackCoords = [4,6];
+    
+    //human player attacks 
+    humanPlayer.takeTurn(computerPlayer, attackCoords);
+    
+    const resultCell = computerPlayer.battleField.getBattleField()[4][6]
+    
+    expect(resultCell).toBe("M")
+    
+    })
