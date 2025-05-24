3cae58666fd3ec7eb322fd3c8681791a4335f2c4
snooze26 <langdon.douglas26@gmail.com>
2025-05-23 19:16:47 -0500

    feat: added function for testing purposes

--- src/classes/gameManager/players.test.js
+++ src/classes/gameManager/players.test.js
@@ -1,4 +1,4 @@
-const Battle_Field = require('../battleFieldClass.js');
+const BattleField = require('../battleFieldClass.js');
 const Ships = require('../shipClass.js')
 const AI = require('../aiStuff.js');
 const Players = require('./players.js')
@@ -26,24 +26,50 @@ const Players = require('./players.js')
 
 // })
 
-test("Ship has been attacked by player and marked as 'M' " , () => {
+// test("Ship has been attacked by player and marked as 'M' " , () => {
+
+//     const humanPlayer = new Players.HumanPlayer("Tony the tiger"); 
+//     const computerPlayer = new Players.ComputerPlayer("Tony the bot " , "easy");
+    
+//     const testPatrol = new Ships.Patrol(); 
+//     const testPatrolCoords = [[4,4] , [4 , 5]]; 
+      
+//     // place ships on computer player's board 
+//     computerPlayer.battleField.placeShip( testPatrolCoords , testPatrol);
+    
+//     const attackCoords = [4,6];
+    
+//     //human player attacks 
+//     humanPlayer.takeTurn(computerPlayer, attackCoords);
+    
+//     const resultCell = computerPlayer.battleField.getBattleField()[4][6]
+    
+//     expect(resultCell).toBe("M")
+// })
+
+test("Computer ship has been attacked by player and marked as 'H' " , () => {
+
+    const mockRandom = jest.fn()
+    .mockReturnValueOnce(0.1) // x = floor(0.1 * 8) = 8
+    .mockReturnValueOnce(0.2) // y = floor(0.2 * 8) = 1 
 
     const humanPlayer = new Players.HumanPlayer("Tony the tiger"); 
     const computerPlayer = new Players.ComputerPlayer("Tony the bot " , "easy");
     
-    const testPatrol = new Ships.Patrol(); 
-    const testPatrolCoords = [[4,4] , [4 , 5]]; 
-      
+    humanPlayer.battleField = new BattleField(3, 3); 
+    const testD1 = new Ships.Destroyer(); 
+    const testD1Coords = [[2 , 0] , [2 , 1] , [2 , 2]]; 
+
     // place ships on computer player's board 
-    computerPlayer.battleField.placeShip( testPatrolCoords , testPatrol);
-    
-    const attackCoords = [4,6];
+    humanPlayer.battleField.placeShip( testD1Coords , testD1);
+
     
     //human player attacks 
-    humanPlayer.takeTurn(computerPlayer, attackCoords);
+    await computerPlayer.takeTurn(humanPlayer);
     
-    const resultCell = computerPlayer.battleField.getBattleField()[4][6]
-    
-    expect(resultCell).toBe("M")
-    
-    })
+    const resultCell1 = computerPlayer.battleField.getBattleField()[0][1]
+
+    expect(resultCell1).toBe("H")
+    // expect(resultCell3).toBe("H")
+
+})
