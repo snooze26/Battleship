765e0ed3b00b23e7119c5bd693686d7b1043f5b3
snooze26 <langdon.douglas26@gmail.com>
2025-06-13 22:33:16 -0500

    test: finish test case for get player, computer player, and chooseGameMode

--- src/classes/gameManager.test.js
+++ src/classes/gameManager.test.js
@@ -5,29 +5,30 @@ jest.mock('prompt-sync' , () => {
     return () => { 
         return (question) => { 
             if(question.includes("Player 1")) return "Tom";
-            if(question.includes("Player 2")) return "Jerry"; 
+            // if(question.includes("Player 2")) return "Jerry"; 
             if(question.includes("difficulty")) return "Easy";
             return ""; 
         }
     }
 })
-//get player1 and player2
-test("Player 1 and 2 has been retrieved", () => {
-    GameManager.gameManager.chooseGameMode("PVP"); 
+//get player1 and player2 and chooseGameMode
+// test("Player 1 and 2 has been retrieved", () => {
+//     GameManager.gameManager.chooseGameMode("PVP"); 
+//     const p1 = GameManager.gameManager.getPlayer1();
+//     const p2 = GameManager.gameManager.getPlayer2();
+
+//     expect(p1.name).toBe("Tom");
+//     expect(p2.name).toBe("Jerry");
+// })
+// // get player 1 and computer player chooseGameMode 
+test("Game Mode is PVC and there are two human players" , () => {
+    GameManager.gameManager.chooseGameMode("PVC"); 
     const p1 = GameManager.gameManager.getPlayer1();
     const p2 = GameManager.gameManager.getPlayer2();
 
     expect(p1.name).toBe("Tom");
-    expect(p2.name).toBe("Jerry");
-})
-// // chooseGameMode 
-// test("Game Mode is PVP and there are two human players" , () => {
-//     const testMode ="PVP"; 
-
-//     GameManager.gameManager.chooseGameMode(testMode); 
-//     expect(player1.name).toBe("Tom");
-//     expect(player.name).toBe("Jerry");
-// }) 
+    expect(p2.name).toBe("Computer")
+});
 //switchTurn 
 
 //getOpponent 
