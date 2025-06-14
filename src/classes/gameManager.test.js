0cfed0336addf780433f2e90f346f88ac1222c9c
snooze26 <langdon.douglas26@gmail.com>
2025-06-13 22:56:12 -0500

    test: create tets for switchTurn function

--- src/classes/gameManager.test.js
+++ src/classes/gameManager.test.js
@@ -5,7 +5,7 @@ jest.mock('prompt-sync' , () => {
     return () => { 
         return (question) => { 
             if(question.includes("Player 1")) return "Tom";
-            // if(question.includes("Player 2")) return "Jerry"; 
+            if(question.includes("Player 2")) return "Jerry"; // if testing computer player comment this line out 
             if(question.includes("difficulty")) return "Easy";
             return ""; 
         }
@@ -21,16 +21,37 @@ jest.mock('prompt-sync' , () => {
 //     expect(p2.name).toBe("Jerry");
 // })
 // // get player 1 and computer player chooseGameMode 
-test("Game Mode is PVC and there are two human players" , () => {
-    GameManager.gameManager.chooseGameMode("PVC"); 
+// test("Game Mode is PVC and there are two human players" , () => {
+//     GameManager.gameManager.chooseGameMode("PVC"); 
+//     const p1 = GameManager.gameManager.getPlayer1();
+//     const p2 = GameManager.gameManager.getPlayer2();
+
+//     expect(p1.name).toBe("Tom");
+//     expect(p2.name).toBe("Computer")
+// });
+//switchTurn/getCurrentPLayer
+test("Current Player Switchers from p1 to p2 " , () => {
+    GameManager.gameManager.chooseGameMode("PVP"); 
     const p1 = GameManager.gameManager.getPlayer1();
     const p2 = GameManager.gameManager.getPlayer2();
 
-    expect(p1.name).toBe("Tom");
-    expect(p2.name).toBe("Computer")
-});
-//switchTurn 
+    let currentP = GameManager.gameManager.getCurrentPlayer();
 
+    expect(currentP.name).toBe("Tom");
+
+    GameManager.gameManager.switchTurn(); 
+
+    currentP = GameManager.gameManager.getCurrentPlayer(); 
+
+    expect(currentP.name).toBe("Jerry");
+
+    GameManager.gameManager.switchTurn(); 
+
+    currentP = GameManager.gameManager.getCurrentPlayer(); 
+
+    expect(currentP.name).toBe("Tom");
+
+});
 //getOpponent 
 
 //endGame
