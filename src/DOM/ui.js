0224098a58f94a3d959fb43de652c17b513c4b3f
snooze26 <langdon.douglas26@gmail.com>
2025-06-19 15:18:59 -0500

    feat: finish setUpClick and tested function

--- src/DOM/ui.js
+++ src/DOM/ui.js
@@ -11,15 +11,15 @@ function getGameBoard(boardId) {
 
 //creating link to the DOM 
 function createBoard(boardId) { 
-    const board = getGameBoard(boardId);
-    for (let y = 0; y < 10; y++) { 
-        for (let x = 0; x < 10; x++) {
+    const board = getGameBoard(boardId); // creates board 
+    for (let y = 0; y < 10; y++) { // row
+        for (let x = 0; x < 10; x++) { // columns 
         const cell = document.createElement('div');
         console.log(cell)
-        cell.classList.add('cell');
+        cell.classList.add('cell'); // add cell style to cell 
         //gather cell coords 
-        cell.dataset.x = x;
-        cell.dataset.y = y
+        cell.dataset.x = x; 
+        cell.dataset.y = y 
         board.appendChild(cell);
         }
     }
@@ -27,7 +27,21 @@ function createBoard(boardId) {
 
 function setUpCellClick(boardId, callback) { 
     const board = getGameBoard(boardId); 
+    
+    const cells = board.querySelectorAll('.cell'); 
 
+    cells.forEach(cell => {
+        cell.addEventListener("click" , () => { 
+            const x = parseInt(cell.dataset.x); // capture's x value 
+            const y = parseInt(cell.dataset.y); // capture's y value
+ 
+        if(cell.classList.contains("clicked")) return; // returns if cell has alreadt been clicked 
+
+        cell.classList.add("clicked"); // marks cell as clicked 
+
+        callback(x , y , cell); // passes x, y, and cell into whichever function is using setupClick 
+        })
+    });
 }
 
 // document.addEventListener("DOMContentLoaded" , () => {
@@ -36,4 +50,17 @@ function setUpCellClick(boardId, callback) {
 
 // })
 createBoard('#player2Board');
+createBoard('#player1Board');
 
+
+setUpCellClick('#player2Board', (x, y, cell) => {
+    // const hit = gameManager.playerAttack(x, y);  // handles logic
+    // if (hit) cell.classList.add('hit');
+    // else cell.classList.add('miss');
+});
+
+setUpCellClick('#player1Board', (x, y, cell) => {
+    // const hit = gameManager.playerAttack(x, y);  // handles logic
+    // if (hit) cell.classList.add('hit');
+    // else cell.classList.add('miss');
+});
